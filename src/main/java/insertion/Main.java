e355ba6fa84e9abad28bcd25fb7f7ba6437b1c95
ErickMiyagi <erick_miyagi34231@hotmail.com>
2025-06-07 22:40:48 -0300

    feat: add merge sort and compilee program java

--- src/main/java/insertion/Main.java
+++ src/main/java/insertion/Main.java
@@ -16,6 +16,24 @@ public class Main {
         for (int v : vetor){
             System.out.print(" " + v);
         }
+
+        int[] vetor2 = {19,20,1,23,29,10,14,123,12,2};
+
+        System.out.println();
+        System.out.println("Vetor Normal: ");
+        for (int i : vetor2){
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println("Merge Sort: ");
+        mergeSortRecursivo(0, vetor2.length, vetor2);
+
+        for (int i : vetor2){
+            System.out.print(" " + i);
+        }
+        System.out.println();
+
     }
 
     public static void insertionSort(int[] v){
@@ -35,37 +53,39 @@ public class Main {
     public static void mergeSortRecursivo(int p, int r, int[] v){
         if(p < r - 1){
             int q = (p + r) / 2;
-            mergeSortRecursivo(p,q,v);
-            mergeSortRecursivo(q,r,v);
-            mergeSortIntercala(p,q,r,v);
+            mergeSortRecursivo(p, q, v);
+            mergeSortRecursivo(q, r, v);
+            mergeSortIntercala(p, q, r, v);
         }
     }
 
     public static void mergeSortIntercala(int p, int q, int r, int[] v){
-        int i,j,k;
-        int[] w;
+        int i = p;
+        int j = q;
+        int k = 0;
 
-        i = p;
-        j = q;
-        k = 0;
+        int[] w = new int[r - p];
 
-        while ((i < q) && (k < r)){
-            if(v[i] <= v[j]){
+        while (i < q && j < r){
+            if (v[i] <= v[j]){
                 w[k++] = v[i++];
             } else {
                 w[k++] = v[j++];
             }
         }
-        while(i < q){
+
+        while (i < q){
             w[k++] = v[i++];
         }
-        while(j < r){
+
+        while (j < r){
             w[k++] = v[j++];
         }
-        for (i = p; i < r; i++) {
+
+        for (i = p; i < r; i++){
             v[i] = w[i - p];
         }
     }
 
-    
+    public static void quickSort()
 }
