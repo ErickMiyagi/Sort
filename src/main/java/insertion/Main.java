791b08240826f1e4cc6fbe9310d95767a7d7f7b0
ErickMiyagi <erick_miyagi34231@hotmail.com>
2025-06-07 22:11:51 -0300

    Inicializando Repositorio

---
+++ src/main/java/insertion/Main.java
@@ -0,0 +1,71 @@
+package insertion;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] vetor = {12, 11, 13, 5, 6};
+
+        System.out.println("Vetor normal: ");
+        for (int v : vetor){
+            System.out.print(" " + v);
+        }
+
+        System.out.println();
+        insertionSort(vetor);
+
+        System.out.println("Insertion Sort: ");
+        for (int v : vetor){
+            System.out.print(" " + v);
+        }
+    }
+
+    public static void insertionSort(int[] v){
+        int j,x;
+        for (int i = 1; i < v.length; i++) {
+            x = v[i];
+            j = i - 1;
+
+            while (j >= 0 && v[j] > x){
+                v[j + 1] = v[j];
+                j = j - 1;
+            }
+            v[j + 1] = x;
+        }
+    }
+
+    public static void mergeSortRecursivo(int p, int r, int[] v){
+        if(p < r - 1){
+            int q = (p + r) / 2;
+            mergeSortRecursivo(p,q,v);
+            mergeSortRecursivo(q,r,v);
+            mergeSortIntercala(p,q,r,v);
+        }
+    }
+
+    public static void mergeSortIntercala(int p, int q, int r, int[] v){
+        int i,j,k;
+        int[] w;
+
+        i = p;
+        j = q;
+        k = 0;
+
+        while ((i < q) && (k < r)){
+            if(v[i] <= v[j]){
+                w[k++] = v[i++];
+            } else {
+                w[k++] = v[j++];
+            }
+        }
+        while(i < q){
+            w[k++] = v[i++];
+        }
+        while(j < r){
+            w[k++] = v[j++];
+        }
+        for (i = p; i < r; i++) {
+            v[i] = w[i - p];
+        }
+    }
+
+    
+}
